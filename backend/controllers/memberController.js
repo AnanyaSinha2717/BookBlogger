cde4095e65bc2a66600c2a85813ac0ef4f31bbea
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2024-09-07 16:57:41 +0530

    build member schema and connect to db

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -1,87 +1,116 @@
-const Member = require('../models/membermodel')
-const mongoose = require('mongoose')
+const Member = require("../models/membermodel");
+const mongoose = require("mongoose");
+const cloudinary = require("cloudinary").v2;
+const formidable = require("formidable");
 
-
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // get all members
 const getMembers = async (req, res) => {
-    const allmembers = await Member.find({}).sort({ createdAt: -1 })
+  const allmembers = await Member.find({}).sort({ createdAt: -1 });
 
-    res.status(200).json(allmembers)
-}
+  res.status(200).json(allmembers);
+};
 
 //get one member
 const getMember = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Member not found" });
+  }
 
-    const { id } = req.params
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "Member not found" })
-    }
+  const onemember = await Member.findById(id);
 
-    const onemember = await Member.findById(id)
+  if (!onemember) {
+    return res.status(404).json({ message: "Member not found" });
+  }
 
-    if (!onemember) {
-        return res.status(404).json({ message: "Member not found" })
-    }
-
-    res.status(200).json(onemember)
-}
+  res.status(200).json(onemember);
+};
 
 // add new member
 const addMember = async (req, res) => {
-    const { memberName, branch, year, birthday, image } = req.body
+  try {
+    const form = formidable.formidable({});
 
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        next(err);
+        return;
+      }
 
-    try {
-        const member = await Member.create({ memberName, branch, year, birthday, image })
-        res.status(200).json(member)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+    //   console.log(files);
+    //   console.log(fields);
 
-}
+      const { memberName, branch, year, birthday} = fields;
+      const {image} = files;
 
+    //   upload image
+        const uploadImage = await cloudinary.uploader.upload(image);
+        console.log(uploadImage);
+
+        const member = await Member.create({
+          memberName,
+          branch,
+          year,
+          birthday,
+          image: uploadImage.secure_url,
+        });
+        res.status(200).json(member);
+    //   res.status(200);
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // remove member
 const removeMember = async (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "Member not found" })
-    }
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Member not found" });
+  }
 
-    const member = await Member.findByIdAndDelete(id)
+  const member = await Member.findByIdAndDelete(id);
 
-    if (!member) {
-        return res.status(404).json({ error: "Member not found" })
-    }
+  if (!member) {
+    return res.status(404).json({ error: "Member not found" });
+  }
 
-    res.status(200).json(member)
-}
-
+  res.status(200).json(member);
+};
 
 // update member info
 const updateMember = async (req, res) => {
-    const { id } = req.params
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "Member not found" })
-    }
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Member not found" });
+  }
 
-    const member = await Member.findOneAndUpdate({_id: id}, {
-        ...req.body
-    }, {new:true})
+  const member = await Member.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    },
+    { new: true }
+  );
 
-    if (!member) {
-        return res.status(404).json({ error: "Member not found" })
-    }
+  if (!member) {
+    return res.status(404).json({ error: "Member not found" });
+  }
 
-    res.status(200).json(blog)
-}
+  res.status(200).json(blog);
+};
 
 module.exports = {
-    getMembers,
-    getMember,
-    addMember,
-    removeMember,
-    updateMember
-}
+  getMembers,
+  getMember,
+  addMember,
+  removeMember,
+  updateMember,
+};
